bcab68e5939a12b3f6b06c01aa68f7fa31951026
Juho Friman <juho.t.friman@gmail.com>
2022-11-08 17:42:49 +0200

    Always terminate

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod sql;
 use crate::sql::lexer::SqlLexer;
 
 fn main() {
-    match SqlLexer::new("SELECT a, b, c FROM foo") {
+    match SqlLexer::new("SELECT a, b, c FROM foo;") {
         Ok(mut lexer) => {
             while let Some(token) = lexer.next() {
                 println!("{}", token);
