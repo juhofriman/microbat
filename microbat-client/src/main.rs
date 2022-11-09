26cbb9efddbeb03c00893ff9581cb80b122e4a7b
Juho Friman <juho.t.friman@gmail.com>
2022-11-09 19:20:02 +0200

    Using cargo workspaces for client and server

---
+++ microbat-client/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("MICROBAT CLIENT")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_test() {
+        assert!(true)
+    }
+}
--- microbat-server/src/main.rs
+++ microbat-server/src/main.rs
@@ -1,9 +1,14 @@
+mod connect;
 mod sql;
 
 use crate::sql::lexer::SqlLexer;
 use std::env;
 
 fn main() {
+    connect::run()
+}
+
+fn just_lex() {
     let args: Vec<String> = env::args().collect();
     let default_input = String::from("");
     let input = args.get(1).unwrap_or(&default_input);
